0ac63fd05f5b30af672ebee15dc1cb0c857aabbd
Md.Mahadi Hasan <mahadi.hasan163@northsouth.edu>
2021-07-13 18:57:42 +0600

    Splash Screen
    splash screen main design upload

--- Foodie/app/src/main/java/com/cse486/ece/myapplication/MainActivity.java
+++ Foodie/app/src/main/java/com/cse486/ece/myapplication/MainActivity.java
@@ -2,13 +2,23 @@ package com.cse486.ece.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Typeface typeface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.splashtext);
+        typeface = Typeface.createFromAsset(getAssets(),"font/splashfont.otf");
+        textView.setTypeface(typeface);
+
     }
 }
