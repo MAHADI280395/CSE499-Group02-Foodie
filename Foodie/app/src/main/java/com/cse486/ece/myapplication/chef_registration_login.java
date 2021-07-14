b81157610edcba383e51ef84b58106bd626b2166
Md.Mahadi Hasan <mahadi.hasan163@northsouth.edu>
2021-07-14 19:17:08 +0600

    Chef Login Screen
    main design uploaded

---
+++ Foodie/app/src/main/java/com/cse486/ece/myapplication/chef_registration_login.java
@@ -0,0 +1,14 @@
+package com.cse486.ece.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chef_registration_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_registration_login);
+    }
+}
